f8711e74fa8c34fcf7f5b4e9dcc1d428fc6d96cf
Shanu Shikhar <thisisshanushikhar@gmail.com>
2022-04-03 13:14:09 +0530

    basic practise project setup

--- src/App.js
+++ src/App.js
@@ -1,38 +1,24 @@
 import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
-import MainHeader from "./components/MainHeader";
-import ProductDetails from "./pages/ProductDetails";
-import Products from "./pages/Products";
-import Welcome from "./pages/Welcome";
+import AllQuotes from "./pages/AllQuotes";
+import NewQuote from "./pages/NewQuote";
+import QuoteDetail from "./pages/QuoteDetail";
 
 export default function App() {
   return (
-    <div>
-      <MainHeader />
-      <main>
-        <Switch>
-          <Route path="/" exact>
-            <Redirect to="/welcome" />
-          </Route>
-          <Route path="/welcome">
-            <Welcome />
-          </Route>
-          <Route path="/products" exact>
-            <Products />
-          </Route>
-          <Route path="/products/:productID">
-            <ProductDetails />
-          </Route>
-        </Switch>
-      </main>
-    </div>
+    <Switch>
+      <Route path="/" exact>
+        <Redirect to="/Quotes" />
+      </Route>
+      <Route path="/Quotes" exact>
+        <AllQuotes />
+      </Route>
+      <Route path="/Quotes/:quoteId">
+        <QuoteDetail />
+      </Route>
+      <Route path="/new-quote">
+        <NewQuote />
+      </Route>
+    </Switch>
   );
 }
-
-// our-domain.com/welcome => Welcome component
-// our-domain.com/products => Products component
-// our-domain.com/product-details/<anything> => ProductDetails
-
-// if path is matching then both page will open in same page so to avoid we use exact and switch
-// switch makes sure that only 1 router will be active at 1 thing and its based on which matches 1st
-// sometimes we want to active multiple routes at same time
