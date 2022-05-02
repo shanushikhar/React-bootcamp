c6e848a3add35bb244708dc557818dba28592177
Shanu Shikhar <thisisshanushikhar@gmail.com>
2022-05-02 12:16:05 +0530

    added more routing examples

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,11 @@
 import React from "react";
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_DATA = [
+  { id: "1", author: "Bang", text: "Learn React" },
+  { id: "2", author: "Sumakar", text: "Learn Node" },
+];
 
 export default function AllQuotes() {
-  return <div>AllQuotes</div>;
+  return <QuoteList quotes={DUMMY_DATA} />;
 }
