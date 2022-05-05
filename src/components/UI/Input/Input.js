4c692c51339ed4b688223a1a6ade7f708d04c786
Shanu Shikhar <thisisshanushikhar@gmail.com>
2022-05-05 17:24:42 +0530

    Added dynamic input field

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,24 @@
+import classes from "../../Login/Login.module.css";
+
+const Input = (props) => {
+  // we have to use props only to make this input field dynamic
+  // if we use context api then it will be relevant to particular field's context
+  return (
+    <div
+      className={`${classes.control} ${
+        props.isValid === false ? classes.invalid : ""
+      }`}
+    >
+      <label htmlFor={props.id}>{props.name}</label>
+      <input
+        type={props.type}
+        id={props.id}
+        value={props.value}
+        onChange={props.fieldChangeHandler}
+        onBlur={props.validateFormHandler}
+      />
+    </div>
+  );
+};
+
+export default Input;
