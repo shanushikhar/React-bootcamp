a7e0c444d71faa283369aace0700fff6d2ebf661
Shanu Shikhar <thisisshanushikhar@gmail.com>
2022-08-02 15:54:29 +0530

    Added React.memo for component optimization

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
-import { Fragment, useContext } from "react";
-import Cart from "./components/Cart/Cart";
+import React, { useState } from "react";
 
-import Header from "./components/Layout/Header";
-import Meals from "./components/Meals/Meals";
-import ModalContext from "./store/modal-context";
+import Button from "./components/UI/Button/Button";
+import DemoOutput from "./components/Demo/DemoOutput";
+import "./App.css";
 
 function App() {
-  const context = useContext(ModalContext);
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  console.log("APP RUNNING");
+
+  /**
+   *  it will save the function in React's internal storage
+      and we'll always reuse that same function object
+      when this component function executes.
+   */
+
+  /** no need to pass setShowParagraph as a dependency array
+       * with help of Use Callback probably
+          that this will never change so that this will always
+          be the same function object.
+       */
+
+  /** so [] means
+   * it has no dependencies
+      and the data for always the same function object
+      should be reused when the app component rerenders.
+  */
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  };
 
   return (
-    <Fragment>
-      {context.modalIsOpen && <Cart />}
-      <Header />
-      <main>
-        <Meals />
-      </main>
-    </Fragment>
+    <div className="app">
+      <h1>Hi there!</h1>
+      <DemoOutput show={false} />
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
+    </div>
   );
 }
 
