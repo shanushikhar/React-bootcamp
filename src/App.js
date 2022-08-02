8e68f632e84597218d06264e2467ad7d71d51a10
Shanu Shikhar <thisisshanushikhar@gmail.com>
2022-08-02 19:39:12 +0530

    Added class-based component in action

--- src/App.js
+++ src/App.js
@@ -1,29 +1,9 @@
-import React, { useState, useCallback, useMemo } from "react";
-
-import "./App.css";
-import DemoList from "./components/Demo/DemoList";
-import Button from "./components/UI/Button/Button";
+import UserFinder from './components/UserFinder';
 
 function App() {
-  const [listTitle, setListTitle] = useState("My List");
-
-  const changeTitleHandler = useCallback(() => {
-    setListTitle("New Title");
-  }, []);
-  /**
- * we always create a new array here.
-    So even if it's technically a similar array
-    with the same values and the same order,
-    it's still technically a new array in memory,
-    and that is treated as a brand new object
- */
-
-  const memoisedArray = useMemo(() => [5, 3, 1, 10, 9], []);
-
   return (
-    <div className="app">
-      <DemoList title={listTitle} items={memoisedArray} />
-      <Button onClick={changeTitleHandler}>Change List Title</Button>
+    <div>
+      <UserFinder />
     </div>
   );
 }
