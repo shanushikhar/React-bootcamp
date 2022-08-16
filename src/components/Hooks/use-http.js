bd63b3b8ded4f04d855887af5c2f150dced8469b
Shanu Shikhar <thisisshanushikhar@gmail.com>
2022-08-16 17:48:55 +0530

    Added custom-hook v1 to GET data

---
+++ src/components/Hooks/use-http.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+const useHttp = (reqConfig, applyData) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(reqConfig.url, {
+        method: reqConfig.method ? reqConfig.method : "GET",
+        headers: reqConfig.headers ? reqConfig.headers : {},
+        body: reqConfig.body ? JSON.stringify(reqConfig.body) : null,
+      });
+
+      if (!response.ok) {
+        throw new Error("Request failed!");
+      }
+
+      const data = await response.json();
+      //console.log("data ", data);
+
+      applyData(data);
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
+    }
+    setIsLoading(false);
+  };
+
+  return {
+    isLoading,
+    error,
+    sendRequest,
+  };
+};
+
+export default useHttp;
