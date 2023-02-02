26dd586bca4cd1d7d7a8fa28674e515f0d2cd2f9
Shanu Shikhar <thisisshanushikhar@gmail.com>
2023-02-02 20:22:56 +0530

    Added cart logic

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,31 @@
-import Card from '../UI/Card';
-import classes from './Cart.module.css';
-import CartItem from './CartItem';
+import { useSelector } from "react-redux";
+import Card from "../UI/Card";
+import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
+  const itemsInCart = useSelector((state) => state.cart.items);
+  const totalPrice = useSelector((state) => state.cart.totalPrice);
+  const totalAmount = useSelector((state) => state.cart.totalItem);
+
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
-        <CartItem
-          item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
-        />
+        {itemsInCart.map((item) => {
+          return (
+            <CartItem
+              key={item.id}
+              item={{
+                id: item.id,
+                title: item.title,
+                quantity: item.quantity,
+                total: item.totalPrice,
+                price: item.price,
+              }}
+            />
+          );
+        })}
       </ul>
     </Card>
   );
--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,14 +1,34 @@
-import classes from './CartItem.module.css';
+import classes from "./CartItem.module.css";
+
+import { useDispatch } from "react-redux";
+import { cartAddActions, removeCartActions } from "../store";
 
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+  const dispatch = useDispatch();
+
+  const { id, title, quantity, total, price } = props.item;
+
+  const addCartHandeler = () => {
+    dispatch(
+      cartAddActions({
+        id,
+        title,
+        price,
+      })
+    );
+  };
+
+  const removeCartHandler = () => {
+    console.log({ id });
+    dispatch(removeCartActions(id));
+  };
 
   return (
     <li className={classes.item}>
       <header>
         <h3>{title}</h3>
         <div className={classes.price}>
-          ${total.toFixed(2)}{' '}
+          ${total.toFixed(2)}{" "}
           <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
         </div>
       </header>
@@ -17,8 +37,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={removeCartHandler}>-</button>
+          <button onClick={addCartHandeler}>+</button>
         </div>
       </div>
     </li>
