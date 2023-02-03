f4a6cf883822471df33931c1a069f794866ade9f
Shanu Shikhar <thisisshanushikhar@gmail.com>
2023-02-03 13:19:25 +0530

    Added async request via components using useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,79 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import Notification from "../src/components/UI/Notification";
+import { uiAction } from "./components/store/ui-slice";
+import { current } from "@reduxjs/toolkit";
 
+let isFirstTime = true;
+
+// status, title, message
 function App() {
+  const cartData = useSelector((state) => state.cart.items);
+  const notification = useSelector((state) => state.myui.uiNotification);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const asyncFun = async () => {
+      dispatch(
+        uiAction({
+          status: "pending",
+          title: "Request pending",
+          message: "Request is one the way",
+        })
+      );
+      const data = await fetch(
+        "https://hackathon-2639b-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cartData),
+        }
+      );
+      if (!data.ok) {
+        throw new Error();
+      }
+      // const res = await data.json();
+      dispatch(
+        uiAction({
+          status: "success",
+          title: "Request success",
+          message: "Request is completed",
+        })
+      );
+    };
+
+    if (isFirstTime) {
+      isFirstTime = false;
+      return;
+    }
+
+    asyncFun().catch((error) => {
+      dispatch(
+        uiAction({
+          status: "error",
+          title: "Request failed",
+          message: "Something went wrong...",
+        })
+      );
+    });
+  }, [cartData, dispatch]);
+
   return (
-    <Layout>
-      <Cart />
-      <Products />
-    </Layout>
+    <>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        <Cart />
+        <Products />
+      </Layout>
+    </>
   );
 }
 
--- src/components/store/index.js
+++ src/components/store/index.js
@@ -1,55 +1,12 @@
-import { createSlice, configureStore, current } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 
-const initialState = {
-  items: [],
-  totalItem: 0,
-};
-
-const cartSlice = createSlice({
-  name: "cart",
-  initialState,
-  reducers: {
-    addCartHandler(state, action) {
-      const itemToAdd = action.payload;
-      state.totalItem++;
-      const isItemPresent = state.items.find((item) => item.id == itemToAdd.id);
-      //   console.log(JSON.stringify(isItemPresent, undefined, 2));
-      console.log(current(state.items));
-      if (!isItemPresent) {
-        state.items.push({
-          id: itemToAdd.id,
-          title: itemToAdd.title,
-          quantity: 1,
-          totalPrice: itemToAdd.price,
-          price: itemToAdd.price,
-        });
-      } else {
-        isItemPresent.quantity++;
-        isItemPresent.totalPrice =
-          isItemPresent.totalPrice + isItemPresent.price;
-      }
-    },
-    removeCartHandler(state, action) {
-      const itemId = action.payload;
-      state.totalItem--;
-      const isItemPresent = state.items.find((item) => item.id == itemId);
-      if (isItemPresent.quantity == 1) {
-        state.items = state.items.filter((item) => item.id !== itemId);
-      } else {
-        isItemPresent.quantity--;
-        isItemPresent.totalPrice =
-          isItemPresent.totalPrice - isItemPresent.price;
-      }
-    },
-  },
-});
-
-export const cartAddActions = cartSlice.actions.addCartHandler;
-export const removeCartActions = cartSlice.actions.removeCartHandler;
+import cartSlice from "./cart-slice";
+import uiSlice from "./ui-slice";
 
 const store = configureStore({
   reducer: {
-    cart: cartSlice.reducer,
+    cart: cartSlice,
+    myui: uiSlice,
   },
 });
 
