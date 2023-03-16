8418e5ad0b9422951d3ed0fff4638a0ddac6beac
Shanu Shikhar <thisisshanushikhar@gmail.com>
2023-03-16 12:31:02 +0530

    Added edit event

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,15 +1,35 @@
-import { Form, useNavigate } from "react-router-dom";
+import {
+  Form,
+  useActionData,
+  json,
+  redirect,
+  useNavigate,
+  useNavigation,
+} from "react-router-dom";
 
 import classes from "./EventForm.module.css";
 
 function EventForm({ method, event }) {
+  const actionData = useActionData();
+
   const navigate = useNavigate();
+
+  const navigation = useNavigation();
+
+  const navigationState = navigation.state === "submitting";
   function cancelHandler() {
     navigate("..");
   }
 
   return (
-    <Form method="post" className={classes.form}>
+    <Form method={method} className={classes.form}>
+      {actionData && actionData.errors && (
+        <ul>
+          {Object.values(actionData.errors).map((error) => (
+            <li key={error}>{error}</li>
+          ))}
+        </ul>
+      )}
       <p>
         <label htmlFor="title">Title</label>
         <input
@@ -54,10 +74,49 @@ function EventForm({ method, event }) {
         <button type="button" onClick={cancelHandler}>
           Cancel
         </button>
-        <button>Save</button>
+        <button disabled={navigationState}>
+          {navigationState ? "Submitting" : "Save"}
+        </button>
       </div>
     </Form>
   );
 }
 
 export default EventForm;
+
+export async function action({ request, params }) {
+  const data = await request.formData();
+  const method = request.method;
+  const eventData = {
+    title: data.get("title"),
+    image: data.get("image"),
+    date: data.get("date"),
+    description: data.get("description"),
+  };
+  console.log({ method }, { eventData });
+  let url = "http://localhost:8080/events";
+
+  if (method === "PATCH") {
+    const eventId = params.eventId;
+    console.log({ eventId });
+    url = "http://localhost:8080/events/" + eventId;
+  }
+
+  const response = await fetch(url, {
+    method: method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(eventData),
+  });
+
+  if (response.status === 422) {
+    return response;
+  }
+
+  if (!response.ok) {
+    throw json({ message: "Could not save event." }, { status: 500 });
+  }
+
+  return redirect("/events");
+}
--- src/pages/NewEvent.js
+++ src/pages/NewEvent.js
@@ -2,36 +2,36 @@ import { json, redirect } from "react-router";
 import EventForm from "../components/EventForm";
 
 function NewEventPage() {
-  return <EventForm />;
+  return <EventForm method="POST" />;
 }
 
 export default NewEventPage;
 
-export async function action({ request, params }) {
-  const dataFromForm = await request.formData();
-  const eventData = {
-    title: dataFromForm.get("title"),
-    image: dataFromForm.get("image"),
-    date: dataFromForm.get("date"),
-    description: dataFromForm.get("description"),
-  };
+// export async function action({ request, params }) {
+//   const dataFromForm = await request.formData();
+//   const eventData = {
+//     title: dataFromForm.get("title"),
+//     image: dataFromForm.get("image"),
+//     date: dataFromForm.get("date"),
+//     description: dataFromForm.get("description"),
+//   };
+//   console.log({ eventData });
+//   const res = await fetch("http://localhost:8080/events", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(eventData),
+//   });
+//   console.log({ res });
+//   if (!res.ok) {
+//     throw json(
+//       {
+//         message: "Could not submit the req",
+//       },
+//       { status: 403 }
+//     );
+//   }
 
-  const res = await fetch("http://localhost:8080/events", {
-    method: "post",
-    headers: {
-      "Content-type": "application/json",
-    },
-    body: JSON.stringify(eventData),
-  });
-
-  if (!res.ok) {
-    throw json(
-      {
-        message: "Could not submit the req",
-      },
-      { status: 403 }
-    );
-  }
-
-  return redirect("/events");
-}
+//   return redirect("/events");
+// }
